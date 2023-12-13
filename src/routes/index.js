2012ecbc6539b880481f963ea98e62208291fe7c
joviabebeto <brigittetuyishimire5@gmail.com>
2023-12-13 09:06:30 +0200

    middlrware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import userroutes from "./userroutes"
+import jobRoutes from './jobRoutes'
 
 const router=express.Router()
 
 router.use("/user",userroutes)
+router.use("/job",jobRoutes)
+
 export default router
