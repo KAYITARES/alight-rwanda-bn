8e4219571bab3dea721d29ebce29b6f1153e2803
joviabebeto <brigittetuyishimire5@gmail.com>
2023-12-14 11:51:18 +0200

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import userroutes from "./userroutes"
-import jobRoutes from './jobRoutes'
-import jobApplyRoutes from "./jobApplyRoutes"
+import userroutes from "./userroutes";
+import jobRoutes from './jobRoutes';
+
 
 const router=express.Router()
 
 router.use("/user",userroutes)
 router.use("/job",jobRoutes);
-router.use("/jobApply",jobApplyRoutes)
 
 export default router
