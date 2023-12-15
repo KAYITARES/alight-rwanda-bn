dace9c3b838d19c75bab1a547f47ee54ab21bfe6
kagabojaphet <iradukundajaphet59@gmail.com>
2023-12-15 09:41:37 +0200

    add

--- src/controller/stockcontroller.js
+++ src/controller/stockcontroller.js
@@ -12,6 +12,59 @@ class StockController{
             return successmessage(res,201,`Product successfuly Posted`,stock)
         }
     }
+
+    static async GetAllProduct(req,res){
+        const stock=await STOCK.find()
+        if(!stock){
+            return errormessage(res,401,`Product not found`)
+        }
+        else{
+            return successmessage(res,201,`Product ${stock.length} successfuly retrieved`,stock)
+        }
+    }
+
+    static async DeleteAllProduct(req,res){
+        const stock=await STOCK.deleteMany()
+        if(!stock){
+            return errormessage(res,401,`Product not deleted`)
+        }
+        else{
+            return successmessage(res,201,`Product successfuly deleted`)
+        }
+    }
+
+    static async GetOneProduct(req,res){
+        const id=req.params.id
+        const stock=await STOCK.findById(id)
+        if(!stock){
+            return errormessage(res,401,`Product with id ${id} not found`)
+        }
+        else{
+            return successmessage(res,201,`Product successfuly retrieved`,stock)
+        }
+    }
+
+    static async DeleteOneProduct(req,res){
+        const id=req.params.id
+        const stock=await STOCK.findByIdAndDelete(id)
+        if(!stock){
+            return errormessage(res,401,`Product with id ${id} not deleted`)
+        }
+        else{
+            return successmessage(res,201,`Product successfuly deleted`)
+        }
+    }
+
+    static async UpdateProduct(req,res){
+        const id=req.params.id
+        const stock=await STOCK.findByIdAndUpdate(id,req.body,{new:true})
+        if(!stock){
+            return errormessage(res,401,`Product with id ${id} not found`)
+        }
+        else{
+            return successmessage(res,201,`Product successfuly updated`,stock)
+        }
+    }
 }
 
 export default StockController
--- src/routes/stockroutes.js
+++ src/routes/stockroutes.js
@@ -4,5 +4,10 @@ import StockController from "../controller/stockcontroller";
 const router=express.Router()
 
 router.post("/",StockController.PostProduct)
+router.get("/",StockController.GetAllProduct)
+router.delete("/",StockController.DeleteAllProduct)
+router.get("/:id",StockController.GetOneProduct)
+router.delete("/:id",StockController.DeleteOneProduct)
+router.patch("/:id",StockController.UpdateProduct)
 
 export default router
