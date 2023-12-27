6493df151a1abaa5073c6250922f56f8d43a91b7
kagabojaphet <iradukundajaphet59@gmail.com>
2023-12-27 17:26:45 +0200

    add

--- src/utils/stockemail.js
+++ src/utils/stockemail.js
@@ -1,1 +1,13 @@
 import nodemailer from "nodemailer";
+
+const stockEmail=async(userinfo,productData)=>{
+    let transport=nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        port:465,
+        secure:true,
+        auth:{
+            user:process.env.EMAIL,
+            pass:process.env.PASSWORD
+        },
+    })
+}
