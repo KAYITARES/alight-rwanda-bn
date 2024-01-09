21942b0ebf7af946d12e308bcc0ce8c034648f0a
joviabebeto <brigittetuyishimire5@gmail.com>
2024-01-09 09:08:47 +0200

    jobs

--- src/routes/applyroutes.js
+++ src/routes/applyroutes.js
@@ -7,6 +7,6 @@ router.post("/:id",ApplyJobController.ApplyOneJob);
 router.get("/",ApplyJobController.getAllApplyjob);
 router.get("/:id",ApplyJobController.getOnejobApply);
 router.delete("/",ApplyJobController.deleteAlljobapply);
-router.delete("/",ApplyJobController.deleteOnejobapply)
+router.delete("/",ApplyJobController.deleteOnejobapply);
 
 export default router
