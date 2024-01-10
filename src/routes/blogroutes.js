a78b5b34e86dcb3d4a4fadcb631c6d2a1402a93d
kagabojaphet <iradukundajaphet59@gmail.com>
2024-01-10 15:41:21 +0200

    add

--- src/routes/blogroutes.js
+++ src/routes/blogroutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import BlogController from "../controller/blogcontroller";
 import verifyAccess from "../middlewares/verifyaccess"
+import BLOG from "../model/blog";
 
 const router=express.Router()
 
-router.post("/",BlogController.CreateBlog)
+router.post("/",verifyAccess("admin"),BlogController.CreateBlog)
 router.get("/",BlogController.GetAllBlog)
-router.delete("/",BlogController.DeleteAllBLogs)
+router.delete("/",verifyAccess("admin"),BlogController.DeleteAllBLogs)
+router.get("/:id",BlogController.getOneBlog)
+router.delete("/:id",verifyAccess("admin"),BlogController.deleteOneBlog)
+router.patch("/:id",verifyAccess("admin"),BlogController.updateBlog)
 router.put("/Like/:id",verifyAccess("user"),BlogController.Like)
 router.put("/Dislike/:id",verifyAccess("user"),BlogController.DisLike)
 
