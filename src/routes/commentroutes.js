2bba8a51c762cf41683fe628b2225ecd2ed52d5e
kagabojaphet <iradukundajaphet59@gmail.com>
2024-01-12 12:37:42 +0200

    add

--- src/routes/commentroutes.js
+++ src/routes/commentroutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import CommentController from "../controller/commentcontroller";
 import verifyAccessUsers from "../middlewares/verifyaccessusers";
+import VerifyAccess from "../middlewares/verifyaccess";
 
 const router=express.Router()
 
 router.post("/:id",verifyAccessUsers,CommentController.CreateComment)
-router.get("/",CommentController.getcomment)
-router.get("/:id",CommentController.getOneComment)
-router.delete("/:id",CommentController.deleteOneComment)
-router.patch("/:id",CommentController.updateComment)
-router.delete("/",CommentController.deleteAllComment)
+router.get("/",verifyAccessUsers,CommentController.getcomment)
+router.get("/:id",verifyAccessUsers,CommentController.getOneComment)
+router.delete("/:id",VerifyAccess("admin"),CommentController.deleteOneComment)
+router.patch("/:id",VerifyAccess("admin"),CommentController.updateComment)
+router.delete("/",VerifyAccess("admin"),CommentController.deleteAllComment)
 
 export default router
--- src/routes/stockroutes.js
+++ src/routes/stockroutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import StockController from "../controller/stockcontroller";
 import verifyAcssess from "../middlewares/verifyaccess";
+import verifyAccessUsers from "../middlewares/verifyaccessusers";
 
 const router=express.Router()
 
 router.post("/import",verifyAcssess("admin"),StockController.ImportProduct)
-router.post("/export",StockController.ExportProduct)
-router.get("/",StockController.GetAllStock)
+router.post("/export",verifyAccessUsers,StockController.ExportProduct)
+router.get("/",verifyAccessUsers,StockController.GetAllStock)
 router.delete("/",verifyAcssess("admin"),StockController.DeleteAllStock)
-router.get("/:id",StockController.GetOneProduct)
+router.get("/:id",verifyAccessUsers,StockController.GetOneProduct)
 router.delete("/:id",verifyAcssess("admin"),StockController.DeleteOneProduct)
 router.patch("/:id",verifyAcssess("admin"),StockController.UpdateProduct)
 
